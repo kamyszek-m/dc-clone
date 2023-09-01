2b4af6a8559ed8dd03ee36df52300bf2cc653d7c
kamyszek-m <raholyt@gmail.com>
2023-09-01 11:28:12 +0200

    Added basic themes

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,11 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="flex">
+    <div>
       <UserButton 
         afterSignOutUrl="/"
       />
+      <ModeToggle />
     </div>
   )
 }
